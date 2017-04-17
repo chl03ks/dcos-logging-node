c95d5249f2cd5f03841f7a6cc0f62198de2f3bef
Daniel <dan@hectic.io>
2017-04-17 10:44:50 -0500

    moving options and config order in function

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,8 @@
-const dcosLogging = require('../lib/index')({
+const dcosLogging = require('../lib/index')({}, {
     headers: {
         'Authorization': 'token=DCOS_ACCESS_TOKEN'
     }
-}, {});
+});
 
 dcosLogging.range.get(null, {skip_prev: 200, limit: 3})
     .then(logs => console.log(logs))
